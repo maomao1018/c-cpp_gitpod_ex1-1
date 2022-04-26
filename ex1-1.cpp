24ff8c39976378e07c30b29d67d344183a93b639
maomao <104436607+maomao1018@users.noreply.github.com>
2022-04-26 21:35:34 +0800

    Create ex1-1.cpp

---
+++ ex1-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    // using namespace is must
+    /*
+    hello , this is my first C / C+t program
+    */
+    printf(" hello 2 In ");
+    std::cout<<"Hello World";
+    return 0;
+}
